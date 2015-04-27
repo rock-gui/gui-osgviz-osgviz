fb8d3c053907472391bf9854bab58fb6ff765d95
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2015-04-27 15:01:35 +0200

    added missing file

---
+++ src/interfaces/WindowInterface.h
@@ -0,0 +1,31 @@
+/*
+ * WindowInterface.h
+ *
+ *  Created on: 17.04.2015
+ *      Author: planthaber
+ */
+
+#ifndef OSGVIZ_SRC_INTERFACES_WINDOWINTERFACE_H_
+#define OSGVIZ_SRC_INTERFACES_WINDOWINTERFACE_H_
+
+namespace osgviz {
+
+class WindowInterface{
+
+public:
+	virtual void enableCameraControl() = 0;
+	virtual void disableCameraControl() = 0;
+
+
+	virtual void setCursorShape(int cursor) = 0;
+	virtual void setCursorPos(int x, int y) = 0;
+
+};
+
+
+
+}
+
+
+
+#endif /* OSGVIZ_SRC_INTERFACES_WINDOWINTERFACE_H_ */
