045921484a50ff1559618d35edb7273d989cb647
Arne Böckmann <arne.boeckmann@dfki.de>
2016-04-28 11:31:52 +0200

    Object: fix indentation

--- src/plugins/Object.cpp
+++ src/plugins/Object.cpp
@@ -18,23 +18,23 @@
 namespace osgviz {
 
 Object::Object():cull_mask(0xffffffff),visible(true){
-	//scaleTransform->setMatrix(osg::Matrix::scale(1.0, 1.0, 1.0));
-	//PositionAttitudeTransform::addChild(scaleTransform);
+    //scaleTransform->setMatrix(osg::Matrix::scale(1.0, 1.0, 1.0));
+    //PositionAttitudeTransform::addChild(scaleTransform);
 }
 
 Object::~Object() {
-	// TODO Auto-generated destructor stub
+
 }
 
 //void Object::setContent(osg::ref_ptr<osg::Node> object) {
-//	this->object = object;
-//	scaleTransform->addChild(object);
-//	//root->addChild(this);
+    //this->object = object;
+    //scaleTransform->addChild(object);
+    ////root->addChild(this);
 //}
 
 //void Object::setRootNode(osg::Group* node){
-//	root = node;
-//	root->addChild(this);
+    //root = node;
+    //root->addChild(this);
 //}
 
 
@@ -67,13 +67,13 @@ void Object::displayName(float font_size){
 
     text->setText(this->getName());
 
-	this->addChild(texttransform);
+    this->addChild(texttransform);
 
 }
 
 void Object::xorCullMask(unsigned int mask) {
-	cull_mask = cull_mask^mask;
-	this->setNodeMask(cull_mask);
+    cull_mask = cull_mask^mask;
+    this->setNodeMask(cull_mask);
 }
 
 void Object::switchCullMask() {
@@ -89,41 +89,41 @@ void Object::switchCullMask() {
 
 
 bool Object::clicked(const int &buttonMask, const osg::Vec2d &cursor, const osg::Vec3d &world, const osg::Vec3d &local, Clickable *object, WindowInterface* window){
-	//printf("%s click world: %.2f,%.2f,%.2f local: %.2f,%.2f,%.2f \n",this->getName().c_str(),world.x(),world.y(),world.z(),local.x(),local.y(),local.z());
-	bool finish = false;
-	if (!clickablecb.empty()){
-		for (std::vector< Clickable* >::iterator it = clickablecb.begin();it != clickablecb.end(); it++){
-			if ((*it)->clicked(buttonMask,cursor,world,local, this, window)){
-				//printf("%s forwarded click\n",this->getName().c_str());
-				finish = true;
-			}
-		}
+    //printf("%s click world: %.2f,%.2f,%.2f local: %.2f,%.2f,%.2f \n",this->getName().c_str(),world.x(),world.y(),world.z(),local.x(),local.y(),local.z());
+    bool finish = false;
+    if (!clickablecb.empty()){
+        for (std::vector< Clickable* >::iterator it = clickablecb.begin();it != clickablecb.end(); it++){
+            if ((*it)->clicked(buttonMask,cursor,world,local, this, window)){
+                //printf("%s forwarded click\n",this->getName().c_str());
+                finish = true;
+            }
+        }
 
-	}
-	return finish;
+    }
+    return finish;
 }
 
 bool Object::dragged(const int &buttonMask, const osg::Vec2d &cursor, const osg::Vec3d &world, const osg::Vec3d &local, Clickable *object, WindowInterface* window){
-	//printf("%s dragged %.2f,%.2f,%.2f\n",this->getName().c_str(),world.x(),world.y(),world.z());
-	bool finish = false;
-	if (!clickablecb.empty()){
-		for (std::vector< Clickable* >::iterator it = clickablecb.begin();it != clickablecb.end(); it++){
-			if ((*it)->dragged(buttonMask,cursor,world,local, this, window)){
-				//printf("%s forwarded dragged\n",this->getName().c_str());
-				finish = true;
-			}
-		}
+    //printf("%s dragged %.2f,%.2f,%.2f\n",this->getName().c_str(),world.x(),world.y(),world.z());
+    bool finish = false;
+    if (!clickablecb.empty()){
+        for (std::vector< Clickable* >::iterator it = clickablecb.begin();it != clickablecb.end(); it++){
+            if ((*it)->dragged(buttonMask,cursor,world,local, this, window)){
+                //printf("%s forwarded dragged\n",this->getName().c_str());
+                finish = true;
+            }
+        }
 
-	}
-	return finish;
+    }
+    return finish;
 }
 
 void Object::addClickableCallback(Clickable* cb) {
-	this->clickablecb.push_back(cb);
+    this->clickablecb.push_back(cb);
 }
 
 void Object::setDirty(){
-	this->setUserValue("dirty",true);
+    this->setUserValue("dirty",true);
 }
 
 void Object::setName(const std::string &name){
