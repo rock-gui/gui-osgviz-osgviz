87d26033d25108839f57113d22ecbab36a53d315
Arne Böckmann <arne.boeckmann@dfki.de>
2016-10-13 11:20:36 +0200

    Added missing file

---
+++ src/modules/viz/Primitives/Primitives/LinesNode.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <osg/Geode>
+#include <osg/Geometry>
+#include <osgViz/Object.h>
+
+namespace osgviz 
+{
+    /** Displays a group of lines*/
+    class LinesNode : public osgviz::Object
+    {
+    public:
+        LinesNode(const osg::Vec4& rgbaColor);
+        void addLine(const osg::Vec3& start, const osg::Vec3& end);
+    private:
+        osg::ref_ptr<osg::Vec4Array> colors;
+        osg::ref_ptr<osg::Vec3Array> vertices;
+        osg::ref_ptr<osg::Geometry> geom;
+        osg::ref_ptr<osg::Geode> geode;
+    };
+    
+}
